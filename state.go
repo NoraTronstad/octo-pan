0128d8214b0b6c8282c45a6137c5210ed37a98ce
NoraTronstad <89384393+NoraTronstad@users.noreply.github.com>
2022-01-25 11:26:20 +0100

    Create state.go

---
+++ state.go
@@ -0,0 +1,3 @@
+func ViewState() string { func ViewState() string {   
+    return "[kylling rev korn mann ---\\ \\__/ _________________/---]"    return "[kylling rev korn mann ---\\ \\__/ _________________/---]"  
+}}  
