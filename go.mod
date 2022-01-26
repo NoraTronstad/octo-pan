1721629b29bde8473e008a3cbae3e2379c8bce6a
norae <noratr@uia.no>
2022-01-26 18:19:46 +0100

    ky

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
+module github.com/NoraTronstad/octo-pan
 
+go 1.17
--- state_test.go
+++ state_test.go
@@ -1,8 +1,9 @@
-import "testing"import "testing"  
+import "testing"  
 func TestViewState(t *testing.T) {func TestViewState(t *testing.T) {  
-    wanted := "[kylling rev korn mann ---\ \__/ _________________/---]"    wanted := "[kylling rev korn mann ---\ \__/ _________________/---]"  
-    state := ViewState();    state := ViewState();  
-    if state != wanted {    if state != wanted {  
-        t.Errorf("Feil, fikk %q, ønsket %q.", state, wanted)         t.Errorf("Feil, fikk %q, ønsket %q.", state, wanted)   
-    }    }
-}}  
+    wanted := "[kylling rev korn mann ---\ \__/ _________________/---]" 
+    state := ViewState();   
+    if state != wanted {  
+        t.Errorf("Feil, fikk %q, ønsket %q.", state, wanted)   
+    }    
+}
+
