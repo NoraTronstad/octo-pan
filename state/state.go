89fc9f5546347875474211d786252508ff9a785f
norae <noratr@uia.no>
2022-01-27 16:31:41 +0100

    mappe

---
+++ state/state.go
@@ -0,0 +1,16 @@
+package state
+
+import (
+    "fmt"
+    "strings"
+    "errors"
+)
+
+var west string = "HS rev korn kylling"
+var boat string = ""
+var east string = ""
+
+
+func ViewState() string {   
+    return "[kylling rev korn mann ---\\ \\__/ _________________/---]"    return "[kylling rev korn mann ---\\ \\__/ _________________/---]"  
+}
