19893b099bc1496d3ddb09285078cd3341f5d05e
norae <noratr@uia.no>
2022-01-27 17:52:55 +0100

    return rev

--- state/state.go
+++ state/state.go
@@ -1,3 +1,17 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+    "errors"
+)
+
+var west string = "HS rev korn kylling"
+var boat string = ""
+var east string = ""
+
 func ViewState() string { func ViewState() string {   
     return "[kylling rev korn mann ---\\ \\__/ _________________/---]"    return "[kylling rev korn mann ---\\ \\__/ _________________/---]"  
-}}  
+}
+
+
--- state/state_test.go
+++ state/state_test.go
@@ -1,9 +1,22 @@
-import "testing"  
+import "testing"
+
 func TestViewState(t *testing.T) { 
     wanted := "[kylling rev korn mann ---\ \__/ _________________/---]" 
     state := ViewState();   
     if state != wanted {  
         t.Errorf("Feil, fikk %q, ønsket %q.", state, wanted)   
-    }    
-}
+    }
+    
+func PutInBoat() String{
+        return "rev"
+    }
+    
+func TestPutInBoat(t *testing.T) {
+        wanted := "rev"
+        var state = PutInBoat()
+        if state != wanted {
+            t.Errorf("Feil, fikk #{state}, ønsket #{wanted}")
+        }
+    }
+
 
